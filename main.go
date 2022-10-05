113e04ccb5ffe607fa014567141bffa38481184f
linweiyuan <root@linweiyuan.com>
2022-10-05 14:15:34 +0800

    config router

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"database/sql"
+
 	"github.com/gin-gonic/gin"
+	"github.com/linweiyuan/goquestionapi/api"
 	"github.com/linweiyuan/goquestionapi/middleware"
+	"github.com/linweiyuan/goquestionapi/router"
+	"github.com/linweiyuan/goquestionapi/sqlc"
 	"github.com/linweiyuan/goquestionapi/util"
 	log "github.com/sirupsen/logrus"
 
@@ -17,8 +22,18 @@ var (
 func init() {
 	config = util.LoadConfig(".")
 
+	sqlDB, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatalf("failed to connect to DB: [%v]", err)
+	}
+
+	db := sqlc.New(sqlDB)
+
 	server = gin.Default()
 	server.Use(middleware.CORS())
+
+	rg := server.Group("/")
+	router.NewQuestionRouter(api.NewQuestionAPI(db)).Setup(rg)
 }
 
 func main() {
---
+++ router/question.go
@@ -0,0 +1,19 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/linweiyuan/goquestionapi/api"
+)
+
+type QuestionRouter struct {
+	api *api.QuestionAPI
+}
+
+func NewQuestionRouter(api *api.QuestionAPI) *QuestionRouter {
+	return &QuestionRouter{api}
+}
+
+func (router *QuestionRouter) Setup(routerGroup *gin.RouterGroup) {
+	group := routerGroup.Group("/questions")
+	group.GET("", router.api.GetQuestions)
+}
